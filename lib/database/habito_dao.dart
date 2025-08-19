f3bd4d938856b070574943f89fef0903ff2300cd
Larissa <lms72@aluno.ifal.edu.br>
2025-08-19 09:27:51 -0300

    Versão de desenvolvimento 3.3

--- lib/database/habito_dao.dart
+++ lib/database/habito_dao.dart
@@ -4,37 +4,16 @@ import 'database_helper.dart';
 class HabitoDAO {
   final DatabaseHelper _dbHelper = DatabaseHelper();
 
-  /// Salva um hábito no banco de dados.
-  /// Retorna o ID da linha inserida.
   Future<int> salvarHabito(Map<String, dynamic> habito) async {
     final db = await _dbHelper.initDB();
     return await db.insert('habitos', habito);
   }
 
-  /// Lista todos os hábitos salvos, ordenados por data decrescente.
   Future<List<Map<String, dynamic>>> listarHabitos() async {
     final db = await _dbHelper.initDB();
     return await db.query(
       'habitos',
       orderBy: 'data DESC',
     );
-    
-  Future<void> salvarHabito(Map<String, dynamic> habito) async {
-    Database db = await DatabaseHelper().initDB();
-    await db.insert('habitos', habito);
-  }
-
-  Future<List<Map<String, dynamic>>> listarHabitos() async {
-    List<Map<String, dynamic>> listaHabitos = [];
-    Database db = await DatabaseHelper().initDB();
-
-    String sql = 'SELECT * FROM habitos ORDER BY data DESC;';
-    var listResult = await db.rawQuery(sql);
-
-    for (var item in listResult) {
-      listaHabitos.add(item);
-    }
-
-    return listaHabitos;
   }
 }
