cfcc31da3e4872a0e0d8fdf1f85bbded386e7474
Larissa <lms72@aluno.ifal.edu.br>
2025-10-12 18:41:04 -0300

    Versão de desenvolvimento 4.4

---
+++ lib/api/humor.api.dart
@@ -0,0 +1,23 @@
+import 'package:dio/dio.dart';
+import '../model/humor_model.dart';
+
+class HumorApi {
+  final dio = Dio();
+  final String baseUrl = 'https://my-json-server.typicode.com/LarissaMaria8715/equilibre-api';
+
+  Future<List<Humor>> findAll() async {
+    List<Humor> listaHumor = [];
+
+    final response = await dio.get('$baseUrl/humor');
+
+    if (response.statusCode == 200) {
+      var listResult = response.data;
+      for (var json in listResult) {
+        Humor humor = Humor.fromJson(json);
+        listaHumor.add(humor);
+      }
+    }
+
+    return listaHumor;
+  }
+}
---
+++ lib/api/usuarios_api.dart
@@ -0,0 +1,25 @@
+import 'package:dio/dio.dart';
+import '../model/user_model.dart';
+
+class UsuariosApi {
+  final dio = Dio();
+  final String baseUrl = 'https://my-json-server.typicode.com/LarissaMaria8715/equilibre-api';
+
+  Future<List<Usuario>> findAll() async {
+    List<Usuario> listaUsuarios = [];
+
+    final response = await dio.get('$baseUrl/usuarios');
+
+    if (response.statusCode == 200) {
+      var listResult = response.data;
+      for (var json in listResult) {
+        Usuario usuario = Usuario.fromJson(json);
+        listaUsuarios.add(usuario);
+      }
+    }
+
+    await Future.delayed(Duration(seconds: 2));
+
+    return listaUsuarios;
+  }
+}
